0d51dfce259897798fd639dff5297d565a76363e
Sumantradas01 <dassumantra36@gmail.com>
2026-09-01 21:32:17 +0530

    Update project

--- src/components/subject_card.py
+++ src/components/subject_card.py
@@ -13,7 +13,7 @@ def subject_card(name, code, section, teacher_name=None, stats=None, footer_call
         <div style="display:flex; gap:8px; flex-wrap:wrap;">
         """
         for icon, label, value in stats:
-            html+= f'<div style="background: #EB459E10; padding:5px 12px; border-radius:12px; font-size:0.9rem">{icon} <b>{value}</b> {label} </div>'
+            html+= f'<div style="background: #64748b; padding:5px 12px; border-radius:12px; font-size:0.9rem">{icon} <b>{value}</b> {label} </div>'
         
         html+= "</div>"
     st.markdown(html, unsafe_allow_html=True)
